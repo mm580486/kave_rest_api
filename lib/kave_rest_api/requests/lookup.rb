e73e6e42dbbad79199bfb77df4c7094b66191aac
Alireza Mohagheghi <mr.alireza19@gmail.com>
2018-10-20 11:55:12 +0330

    bug fix for lookup methods

--- lib/kave_rest_api/requests/lookup.rb
+++ lib/kave_rest_api/requests/lookup.rb
@@ -8,13 +8,13 @@ module KaveRestApi
     validates_length_of :message, :within => 1..140
     validates_format_of :sender, :with => /^\d*$/, :if => Proc.new { !sender.nil? }
     validates_format_of :unixdate, :with => /^\d*$/, :if => Proc.new { !unixdate.nil? }
-    
+
     def initialize(args = {})
       super
       @ACTION_NAME    = [:lookup,@FORMAT].join('.').freeze
       @receptor    = args.fetch(:receptor)
       @receptor    = @receptor.ctsd
-      @valid_receptor = @receptor.is_phone? 
+      @valid_receptor = @receptor.is_phone?
       @token       = args.fetch(:token)
       @token2      = args.fetch(:token2,nil)
       @token3      = args.fetch(:token3,nil)
@@ -22,24 +22,24 @@ module KaveRestApi
       @type        = args.fetch(:type,'sms')
       @response    = ResponseLookup.new
     end
-    
+
     def valid_message?
       @valid_message ||= true
     end
-    
+
     def valid_receptor?
       @valid_receptor
     end
-    
+
     def call
         connection = Faraday.new(url: "#{@API_URL}/verify/") do |faraday|
           faraday.adapter Faraday.default_adapter
           faraday.response @FORMAT.to_sym
         end
-         response = connection.get(@ACTION_NAME, receptor: @receptor , message: @message,localid: @localid,sender: @sender,date: @date,type: @type)
+         response = connection.get(@ACTION_NAME, receptor: @receptor , token: @token , template: @template , type: @type , token2: @token2 , token3: @token3)
          @response.validate(response.body)
     end
-    
+
   end
 
-end 
+end
